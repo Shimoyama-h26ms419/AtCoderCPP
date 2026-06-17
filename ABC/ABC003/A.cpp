b49b9f97c0b86bbb6a1d13dfa15395f491b977a2
Shimoyama Ayumu <shimoyama.h26ms419@gmail.com>
2026-06-18 01:01:51 +0900

    Solved ABC003-A

---
+++ ABC/ABC003/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    cout << 5000 * (N + 1) << '\n';
+
+    return 0;
+}
