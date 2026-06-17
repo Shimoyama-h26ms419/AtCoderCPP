56b0f5db9a0b26ad826d4e00957be86e29e50ac8
Shimoyama Ayumu <shimoyama.h26ms419@gmail.com>
2026-06-18 00:57:30 +0900

    Solved ABC001-A

---
+++ ABC/ABC001/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int H1, H2;
+    cin >> H1 >> H2;
+
+    cout << H1 - H2 << '\n';
+
+    return 0;
+}
