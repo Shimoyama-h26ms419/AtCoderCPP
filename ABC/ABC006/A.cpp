0bb4cba7a3c87a345cc1e2c00d9cedc5656f329e
Shimoyama Ayumu <shimoyama.h26ms419@gmail.com>
2026-06-19 00:40:17 +0900

    Solved ABC006-A

---
+++ ABC/ABC006/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    cout << (N % 3 == 0 ? "YES" : "NO") << '\n';
+
+    return 0;
+}
