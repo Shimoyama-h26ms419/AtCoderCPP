4317236603121ebafca00e41e97a3803ae560a3b
Shimoyama Ayumu <shimoyama.h26ms419@gmail.com>
2026-06-20 23:09:29 +0900

    Solved ABC463-A

---
+++ ABC/ABC463/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int X, Y;
+    cin >> X >> Y;
+
+    cout << (9 * X == 16 * Y ? "Yes" : "No") << '\n';
+
+    return 0;
+}
