66537e948bc089eb2bdcbfdbb8cca672cf102234
Shimoyama Ayumu <shimoyama.h26ms419@gmail.com>
2026-06-22 03:32:55 +0900

    Solved ABC020-A

---
+++ ABC/ABC020/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int Q;
+    cin >> Q;
+
+    cout << (Q == 1 ? "ABC" : "chokudai") << '\n';
+
+    return 0;
+}
