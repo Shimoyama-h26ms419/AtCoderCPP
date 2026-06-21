1215eece115ac42fb1648cace77c3705d2556fff
Shimoyama Ayumu <shimoyama.h26ms419@gmail.com>
2026-06-22 03:34:16 +0900

    Solved ABC021-A

---
+++ ABC/ABC021/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    cout << N << '\n';
+    for (int i = 0; i < N; i++) cout << 1 << '\n';
+
+    return 0;
+}
