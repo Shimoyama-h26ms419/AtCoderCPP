6bfa2a4316af1416d7f3fcbf7c0e2f4980437aac
Shimoyama Ayumu <shimoyama.h26ms419@gmail.com>
2026-06-23 05:26:05 +0900

    Solved ABC023-A

---
+++ ABC/ABC023/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int X;
+    cin >> X;
+
+    cout << X / 10 + X % 10 << '\n';
+
+    return 0;
+}
