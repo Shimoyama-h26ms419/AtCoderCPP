105b727b2d7654fde52cb6ee679afd295f9d6aab
Shimoyama Ayumu <shimoyama.h26ms419@gmail.com>
2026-06-25 02:05:21 +0900

    Solved ABC038-A

---
+++ ABC/ABC038/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string S;
+    cin >> S;
+
+    cout << (S.ends_with("T") ? "YES" : "NO") << '\n';
+
+    return 0;
+}
